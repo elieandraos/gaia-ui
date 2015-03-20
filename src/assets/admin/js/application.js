7ccffd3da06d383cb5f8df1ef1a58c9bc3355351
Elie Andraos <elie@andraos.org>
2015-03-20 18:31:40 +0200

    bootstrap views

---
+++ src/assets/admin/js/application.js
@@ -0,0 +1,64 @@
+var app = function() {
+
+    var init = function() {
+
+        tooltips();
+        toggleMenuLeft();
+        menu();
+        togglePanel();
+        closePanel();
+    };
+
+    var tooltips = function() {
+        $('#toggle-left').tooltip();
+    };
+
+    var togglePanel = function() {
+        $('.actions > .fa-chevron-down').click(function() {
+            $(this).parent().parent().next().slideToggle('fast');
+            $(this).toggleClass('fa-chevron-down fa-chevron-up');
+        });
+    };
+
+    var toggleMenuLeft = function() {
+        $('#toggle-left').bind('click', function(e) {
+            if (!$('.sidebarRight').hasClass('.sidebar-toggle-right')) {
+                $('.sidebarRight').removeClass('sidebar-toggle-right');
+                $('.main-content-wrapper').removeClass('main-content-toggle-right');
+            }
+            $('.sidebar').toggleClass('sidebar-toggle');
+            $('.main-content-wrapper').toggleClass('main-content-toggle-left');
+            e.stopPropagation();
+        });
+    };
+
+    var closePanel = function() {
+        $('.actions > .fa-times').click(function() {
+            $(this).parent().parent().parent().fadeOut();
+        });
+
+    }
+
+    var menu = function() {
+        $("#leftside-navigation .sub-menu > a").click(function(e) {
+            $("#leftside-navigation ul ul").slideUp();
+            if (!$(this).next().is(":visible")) {
+                $(this).next().slideDown();
+            }
+              e.stopPropagation();
+        });
+    };
+    //End functions
+
+
+    //return functions
+    return {
+        init: init
+    };
+}();
+
+//Load global functions
+$(document).ready(function() {
+    app.init();
+
+});
---
+++ src/eandraos/gaia-ui/GaiaUiServiceProvider.php
@@ -0,0 +1,28 @@
+<?php namespace eandraos\gaia-ui;
+
+use Illuminate\Support\ServiceProvider;
+
+class GaiaUiServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //load the admin views and publish them in resources/views/
+        $this->loadViewsFrom(__DIR__ . '/../../views', 'gaia-ui');
+        $this->publishes([
+            __DIR__ . '/../../views' => base_path('resources/views/')
+        ]);
+    }
+
+}
