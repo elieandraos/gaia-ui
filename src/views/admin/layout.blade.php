53440c93f8fbbf6948e68eb2b592a9d5a1124ab5
Elie Andraos <elie.andraos@elieandraos.local>
2015-05-06 18:15:35 +0300

    updated layout file and main css file

--- src/views/admin/layout.blade.php
+++ src/views/admin/layout.blade.php
@@ -68,7 +68,7 @@
                         <a href="javascript:void(0);"><i class="fa fa-file"></i><span>Pages</span><i class="arrow fa fa-angle-right pull-right"></i></a>
                         <ul>
                             <li><a href="/admin/pages/create/"><i class="arrow fa fa-angle-right"></i>Create Page</a></li>
-                            <li><a href="/admin/pages/create/"><i class="arrow fa fa-angle-right"></i>List Pages</a></li>
+                            <li><a href="/admin/pages/"><i class="arrow fa fa-angle-right"></i>List Pages</a></li>
                             <li><a href="/admin/pages/templates/create" id="add-template"><i class="arrow fa fa-angle-right"></i>Create Template</a></li>
                             <li><a href="/admin/pages/templates/" ><i class="arrow fa fa-angle-right"></i>List Templates</a></li>
                         </ul>
